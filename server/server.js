988204ec010303093c10a636ac6e74b541c7edb8
glen <jinx3y@gmail.com>
2018-01-22 23:18:00 +0100

    mongo db set up, gitignore added

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://auth:auth-test@ds111608.mlab.com:11608/graphql-auth';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
